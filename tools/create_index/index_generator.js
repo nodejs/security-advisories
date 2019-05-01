e39bf997c44d8e289bdfdef2455ca47231d1a318
Tierney Cyren <hello@bnb.im>
2019-05-01 18:10:52 -0400

    Ensure indices end with newline

--- tools/create_index/index_generator.js
+++ tools/create_index/index_generator.js
@@ -44,7 +44,7 @@ const createVulnObject = function(identifier, json) {
 }
 
 const writeIndex = function(data, writeDir, filename) {
-  fs.writeFileSync(writeDir + filename + '.json', JSON.stringify(data, null, 2))
+  fs.writeFileSync(writeDir + filename + '.json', JSON.stringify(data, null, 2) + '\n')
 }
 
 module.exports = createIndex
